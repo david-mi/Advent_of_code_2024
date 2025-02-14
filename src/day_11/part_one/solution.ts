6ff386a90017f0179ecabe38d59bbd93267de696
David Michel <david.michel2809@gmail.com>
2025-02-14 08:15:06 +0100

    refacto: add light refacto on d11p1

--- src/day_11/part_one/solution.ts
+++ src/day_11/part_one/solution.ts
@@ -2,48 +2,49 @@ import { setInputLinesToArray } from "../../helpers";
 import { input } from "./inputs/input";
 import { inputExample, inputExample2 } from "./inputs/input_example";
 
+function parseStones(stones: string): number[] {
+  return stones
+    .split(" ")
+    .map(Number);
+}
+
 function isEven(number: number) {
   return number % 2 === 0;
 }
 
-function parseInput(stones: string): number[] {
-  return stones.split(" ").map(Number);
-}
-
-function splitStone(splittableStone: string): number[] {
-  const splitIndex = splittableStone.length / 2;
+function createTwoStonesFromOne(stone: number): number[] {
+  const stoneToString = String(stone);
+  const sliceIndex = stoneToString.length / 2;
 
   return [
-    Number(splittableStone.slice(0, splitIndex)),
-    Number(splittableStone.slice(splitIndex))
+    Number(stoneToString.slice(0, sliceIndex)),
+    Number(stoneToString.slice(sliceIndex))
   ];
 }
 
-function changeStone(stone: number): number[] {
+function replaceStone(stone: number): number[] {
   if (stone === 0) return [1];
 
-  const splittableStone = String(stone);
-  const shouldSplitStone = isEven(splittableStone.length);
-
-  if (shouldSplitStone) {
-    return splitStone(splittableStone);
+  const shouldCreateTwoStones = isEven(String(stone).length);
+  if (shouldCreateTwoStones) {
+    return createTwoStonesFromOne(stone);
   }
 
   return [stone * 2024];
 }
 
-function changeStones(stones: number[]) {
-  return stones.reduce<number[]>((changedStones, stone) => {
-    changedStones.push(...changeStone(stone));
-    return changedStones;
+function replaceStones(stones: number[]) {
+  return stones.reduce<number[]>((replacedStones, stone) => {
+    replacedStones.push(...replaceStone(stone));
+    return replacedStones;
   }, []);
 }
 
 function blink(amount: number, stones: number[]): number[] {
   return amount > 0
-    ? blink(amount - 1, changeStones(stones))
+    ? blink(amount - 1, replaceStones(stones))
     : stones;
 }
-
-const changedStones = blink(25, parseInput(input));
-export const day11PartOneSolution = changedStones.length;
+const BLINKS_AMOUNT = 25;
+const replacedStones = blink(BLINKS_AMOUNT, parseStones(input));
+export const day11PartOneSolution = replacedStones.length;
